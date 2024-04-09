ec54301b24a8f50d5071e02aae53642b0ef710a9
Gloriela Suarez <glo.suacas@gmail.com>
2024-04-08 18:31:42 -0600

    actividad libros

---
+++ tres/libros.js
@@ -0,0 +1,18 @@
+const libros = {
+  
+    titulo: "Mis manos que hablan",
+    autor: "Luis Armando López García",
+    genero: "Educación",
+    añoPublicacion: "2006",
+    editorial: "Trillas"
+     
+  }
+  
+  function infoLibro(){
+    console.log("Título: " + libros.titulo)
+    console.log("Autor: " + libros.autor)
+    console.log("Género: " + libros.genero)
+    console.log("Año de publicación: " + libros.añoPublicacion)
+  }
+  infoLibro()
+  
