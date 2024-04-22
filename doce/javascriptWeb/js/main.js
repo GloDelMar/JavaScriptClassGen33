bdbb69fe4a6546b4489a37d087b3d5a607797e48
Gloriela Suarez <glo.suacas@gmail.com>
2024-04-22 16:13:48 -0600

    la tarea doce

--- doce/javascriptWeb/js/main.js
+++ doce/javascriptWeb/js/main.js
@@ -14,18 +14,51 @@ let koders = [
     {
         name: "Juanito",
         lastname: "De las Pitayas"
+    },
+    {
+        name: "Fulanito",
+        lastname: "Detal"
+    },
+    {
+        name: "Aniv",
+        lastname: "Delarev"
+    },
+    {
+        name: "Susana",
+        lastname: "Distancia"
+    },
+    {
+        name: "Juanelo",
+        lastname: "Pérez"
+    },
+    {
+        name: "Isela",
+        lastname: "Crello"
+    },
+    {
+        name: "Pere",
+        lastname: "Gil"
+    },
+    {
+        name: "Aitor",
+        lastname: "Menta"
+    },
+    {
+        name: "Digna",
+        lastname: "Víbora"
     }
 ]
+
 
 
 let namesList = (entryObjet) => {
     let {name, lastname} = entryObjet
 
     let card = document.createElement("div")
-    card.classList.add("col-4", "card", "blog-card", "mb-3")
+    card.classList.add("list-group")
 
     let cardNames = document.createElement("p")
-    cardNames.classList.add("m-0", "ps-3", "fs-5", "fst-italic")
+    cardNames.classList.add("list-group-item", "col-6", "m-0", "ps-3", "fs-5", "fst-italic")
     let cardFullName = document.createTextNode(`${name} ${lastname}`)
     cardNames.append(cardFullName)
 
@@ -33,7 +66,8 @@ let namesList = (entryObjet) => {
 
     return card
 
-}
+    }
+
 
 const printNames = (blogData, wrapperId) => {
     let wrapper = document.getElementById(wrapperId);
@@ -43,3 +77,4 @@ const printNames = (blogData, wrapperId) => {
     });}
 
     printNames(koders, "card-wrapper")
+   
