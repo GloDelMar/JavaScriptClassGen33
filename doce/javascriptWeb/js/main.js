12ee5b617270c045aeb28c198a175dec6d3955f4
Gloriela Suarez <glo.suacas@gmail.com>
2024-04-21 19:22:39 -0600

    tarea doce

--- doce/javascriptWeb/js/main.js
+++ doce/javascriptWeb/js/main.js
@@ -16,3 +16,30 @@ let koders = [
         lastname: "De las Pitayas"
     }
 ]
+
+
+let namesList = (entryObjet) => {
+    let {name, lastname} = entryObjet
+
+    let card = document.createElement("div")
+    card.classList.add("col-4", "card", "blog-card", "mb-3")
+
+    let cardNames = document.createElement("p")
+    cardNames.classList.add("m-0", "ps-3", "fs-5", "fst-italic")
+    let cardFullName = document.createTextNode(`${name} ${lastname}`)
+    cardNames.append(cardFullName)
+
+    card.append(cardNames)
+
+    return card
+
+}
+
+const printNames = (blogData, wrapperId) => {
+    let wrapper = document.getElementById(wrapperId);
+    blogData.forEach((entry) => {
+      let blogCard = namesList(entry);
+      wrapper.append(blogCard);
+    });}
+
+    printNames(koders, "card-wrapper")
