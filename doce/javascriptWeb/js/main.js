207bee09771e6f007e1d81ba69eb87fc5ee7c8dc
Gloriela Suarez <glo.suacas@gmail.com>
2024-04-21 19:05:50 -0600

    tarea de blog

---
+++ doce/javascriptWeb/js/main.js
@@ -0,0 +1,18 @@
+let koders = [
+    {
+        name:"Israel",
+        lastname:"Salinas Martínez"
+    },
+    {
+        name: "Vianey",
+        lastname: "Díaz"
+    },
+    {
+        name:"Gloriela",
+        lastname:"Suárez"
+    },
+    {
+        name: "Juanito",
+        lastname: "De las Pitayas"
+    }
+]
